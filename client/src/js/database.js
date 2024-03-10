4b1ab4df61f34a8dccdf490bc97aba404e084c57
Mandeep Singh <mandeepsingh@mandeeps-air.lan>
2024-03-10 00:34:25 -0800

    Finished the application. Will make changes if needed as I deploy this on Render.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,28 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// PUT content to DB
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('Data saved to the database', result.value);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// GET all content from DB
+export const getDb = async () => {
+  console.log('GET all data from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+
+  result ? console.log('Data retrieved', result.value) : console.log('Data not found');
+  return result?.value;
+};
 
 initdb();
